758fa88810f5048556e5edb48c254f0859d26053
fimovol <fimovolpe@gmail.com>
2023-01-25 17:23:28 -0500

    feat: components home,user,contact on the routes

--- src/app/principal/principal.module.ts
+++ src/app/principal/principal.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    UserComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports:[
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ContactComponent,
+    UserComponent
   ]
 })
 export class PrincipalModule { }
