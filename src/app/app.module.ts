2cbdf1ea1926d8ace9591671e6d57d9919092813
fimovol <fimovolpe@gmail.com>
2023-01-31 00:03:33 -0500

    refactor: add necesary imports in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalModule } from './principal/principal.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PrincipalModule } from './principal/principal.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PrincipalModule
+    PrincipalModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
